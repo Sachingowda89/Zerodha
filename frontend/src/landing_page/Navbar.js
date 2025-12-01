2f14f9607261ee945fa30537ccba98835ee89686
sachin <sathi4321z@gmail.com>
2025-12-01 15:05:26 +0530

    Updated dashboard URL in navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -8,42 +8,55 @@ function Navbar() {
         <Link className="navbar-brand" to="/">
           <img src="media/images/logo.svg" alt="logo" style={{ width: "25%" }} />
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <form className="d-flex" role="search">
             <ul className="navbar-nav mb-lg-0">
+
+              {/* Updated Register URL */}
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://zerodha-clone-dashboard.vercel.app/register">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="https://zerodha-rho-two.vercel.app/register"
+                >
                   Register
                 </a>
               </li>
+
               <li className="nav-item active">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
+                <Link className="nav-link" to="/about">About</Link>
               </li>
+
               <li className="nav-item active">
-                <Link className="nav-link" to="/product">
-                  Product
-                </Link>
+                <Link className="nav-link" to="/product">Product</Link>
               </li>
+
               <li className="nav-item active">
-                <Link className="nav-link" to="/pricing">
-                  Pricing
-                </Link>
+                <Link className="nav-link" to="/pricing">Pricing</Link>
               </li>
+
               <li className="nav-item active">
-                <Link className="nav-link" to="/support">
-                  Support
-                </Link>
+                <Link className="nav-link" to="/support">Support</Link>
               </li>
+
               <li className="nav-item active">
                 <Link className="nav-link" to="/">
-                  <i class="fa-solid fa-bars"></i>
+                  <i className="fa-solid fa-bars"></i>
                 </Link>
               </li>
+
             </ul>
           </form>
         </div>
